1fa511c9f0aa6ea4bd910f01dcf75ae814451478
Asaf Silver <asaf.silver@post.runi.ac.il>
2025-12-30 11:19:57 +0200

    completed

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -1,5 +1,3 @@
-/** A library of operations on arrays of characters (char values).
- *  The library also features a string comparison method. */
 public class ArrCharOps {
     public static void main(String[] args) {
         String str = "clearly";
@@ -50,116 +48,85 @@ public class ArrCharOps {
     public static boolean equals(char[] arr1, char[] arr2) {
         if (arr1 == null && arr2 == null) return true;
         if (arr1 == null || arr2 == null) return false;
+        if (arr1.length != arr2.length) return false;
         
-        if (arr1.length!=arr2.length)
-        {
-         return false;
-        }
-        for (int i=0; i<arr1.length; i++)
-        {
-         if (arr1[i]!=arr2[i])
-         {
-             return false;
-         }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) return false;
         }
         return true;
     }
 
     public static int indexOf(char[] arr, char ch) {
         if (arr == null) return -1;
-        for (int i=0; i<arr.length; i++)
-         if (arr[i]==ch)
-         {
-             return i;
-         }
-         return -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ch) return i;
+        }
+        return -1;
     }
 
     public static int indexOf(char[] arr, char ch, int fromIndex) {
         if (arr == null) return -1;
-        for (int i = fromIndex; i<arr.length; i++)
-        if (arr[i]==ch)
-        {
-            return i;
+        for (int i = fromIndex; i < arr.length; i++) {
+            if (arr[i] == ch) return i;
         }
         return -1;
     }
 
     public static int lastIndexOf(char[] arr, char ch) {
         if (arr == null) return -1;
-        for (int i=arr.length-1; i>=0; i--)
-        if (arr[i]==ch)
-        {
-            return i;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == ch) return i;
         }
         return -1;
     }
 
     public static char[] concat(char[] arr1, char[] arr2) {
-        if (arr1 == null) arr1 = new char[0];
-        if (arr2 == null) arr2 = new char[0];
+        int len1 = (arr1 == null) ? 0 : arr1.length;
+        int len2 = (arr2 == null) ? 0 : arr2.length;
+        char[] newArr = new char[len1 + len2];
         
-        int totallength = arr1.length+arr2.length;
-        char[] newArr = new char[totallength];
-        for (int i = 0; i<arr1.length;i++)
-        {
+        for (int i = 0; i < len1; i++) {
             newArr[i] = arr1[i];
         }
-        for (int i =0; i<arr2.length; i++)
-        {
-            newArr[arr1.length+i] = arr2[i];
+        for (int i = 0; i < len2; i++) {
+            newArr[len1 + i] = arr2[i];
         }
         return newArr;
     }
 
     public static char[] subArray(char[] arr, int beginIndex, int endIndex) {
-        char[] newArr = new char[endIndex-beginIndex];
-        int j=0;
-        for (int i=beginIndex; i< endIndex; i++)
-        {
-            newArr[j] = arr[i];
-            j++;
+        if (arr == null) return null;
+        char[] newArr = new char[endIndex - beginIndex];
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = arr[beginIndex + i];
         }
         return newArr;
     }
- 
+
     public static long hashCode(char[] arr) {
-        if (arr == null || arr.length == 0) {
-            return 0;
-        }
+        if (arr == null || arr.length == 0) return 0;
         long hash = 0;
         long powerOf7 = 1;
-
         for (int i = arr.length - 1; i >= 0; i--) {
-            hash += (long)arr[i] * powerOf7;
+            hash += (long) arr[i] * powerOf7;
             powerOf7 *= 7;
         }
         return hash;
     }
 
-    public static int compareTo(String str1, String str2) 
-    {
-        int minLength = Math.min(str1.length(), str2.length());
+    public static int compareTo(String str1, String str2) {
+        if (str1 == null || str2 == null) return -2;
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int minLen = Math.min(len1, len2);
 
-        for (int i = 0; i < minLength; i++) {
-            char char1 = str1.charAt(i); 
-            char char2 = str2.charAt(i);
-
-            if (char1 < char2) {
-                return -1;
-            } else if (char1 > char2) {
-                return 1;
-            }
+        for (int i = 0; i < minLen; i++) {
+            if (str1.charAt(i) < str2.charAt(i)) return -1;
+            if (str1.charAt(i) > str2.charAt(i)) return 1;
         }
 
-        int lengthDifference = str1.length() - str2.length();
-        
-        if (lengthDifference < 0) {
-            return -1; 
-        } else if (lengthDifference > 0) {
-            return 1;
-        } else {
-            return 0;
-        }
+        if (len1 < len2) return -1;
+        if (len1 > len2) return 1;
+        return 0;
     }
 }
