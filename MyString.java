19c02136379175a3cfc995a69dc5c573471d9c34
Asaf Silver <asaf.silver@post.runi.ac.il>
2025-12-30 11:15:40 +0200

    completed

--- MyString.java
+++ MyString.java
@@ -16,15 +16,42 @@ public class MyString {
         System.out.println(contains("resignation", "sign")); // true
     }
 
-    /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        if (str == null) {
+            return null;
+        }
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                result += (char) (ch + 32);
+            } else {
+                result += ch;
+            }
+        }
+        return result;
     }
 
-    /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str2.length() == 0) {
+            return true;
+        }
+        if (str2.length() > str1.length()) {
+            return false;
+        }
+
+        for (int i = 0; i <= str1.length() - str2.length(); i++) {
+            boolean match = true;
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i + j) != str2.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                return true;
+            }
+        }
         return false;
     }
 }
